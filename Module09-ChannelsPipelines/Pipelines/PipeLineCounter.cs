d4e2f2d4a760596a24662d8e4bfa48d5179a770d
Lukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2020-08-07 20:49:29 +0200

    Typos

--- Module09-ChannelsPipelines/Pipelines/PipeLineCounter.cs
+++ Module09-ChannelsPipelines/Pipelines/PipeLineCounter.cs
@@ -12,7 +12,7 @@ namespace Pipelines
             await using var stream = await client.GetStreamAsync(uri);
 
             // Calculate how many lines (end of line characters `\n`) are in the network stream
-            // In order to practice, use a pattern where you have the Pipe, Writer and Reader tasks
+            // To practice, use a pattern where you have the Pipe, Writer and Reader tasks
             // Read about SequenceReader<T>, https://docs.microsoft.com/en-us/dotnet/api/system.buffers.sequencereader-1?view=netcore-3.1
             // This struct h has a method that can be very useful for this scenario :)
 
