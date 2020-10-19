13accc6c725479b1d3f2e5f09a2ec69dbb082dde
Szymon Kulec <scooletz@gmail.com>
2020-10-19 11:43:58 +0200

    M06 SmallConcurrentQueueTests added

---
+++ Module06-LowLevel/LowLevelExercises.Tests/SmallConcurrentQueueTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading;
+using NUnit.Framework;
+using SmallConcurrentQueue;
+
+namespace LowLevelExercises.Tests
+{
+    public class SmallConcurrentQueueTests
+    {
+        [Test]
+        public void Load_Test()
+        {
+            const int count = 1000_000;
+
+            var queue = new SmallConcurrentQueue<int>();
+
+            var publisher = new Thread(() =>
+            {
+                for (var i = 0; i < count; i++)
+                {
+                    if (queue.TryEnqueue(i) == false)
+                    {
+                        // queue is full: dummy fast retry
+                        i--;
+                    }
+                }
+            });
+
+            var consumer = new Thread(() =>
+            {
+                for (var i = 0; i < count; i++)
+                {
+                    if (queue.TryDequeue(out var value))
+                    {
+                        Assert.AreEqual(i, value);
+                    }
+                    else
+                    {
+                        // queue is empty: dummy fast retry
+                        i--;
+                    }
+                }
+            });
+
+            publisher.Start();
+            consumer.Start();
+
+            publisher.Join();
+            consumer.Join();
+        }
+    }
+}
